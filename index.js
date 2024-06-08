ec3614169965e44329792d7473ed3a06cc24e8ea
RayBob1993 <>
2024-06-08 13:16:15 +0300

    уроки

--- index.js
+++ index.js
@@ -1,72 +1,9 @@
-import { createServer } from 'node:http';
-import { readFileSync, writeFileSync } from 'node:fs';
+import { Application } from './core/index.js';
+import { usersRouter, catalogRouter } from './src/routers/index.js';
 
-const dbPath = './db.json';
+const server = new Application();
 
-const baseURL = 'http://localhost:8080';
+server.addRouter(usersRouter);
+server.addRouter(catalogRouter);
 
-const server = createServer((request, response) => {
-    const url = request.url;
-    const method = request.method;
-
-    response.writeHead(200, {
-        'Content-Type': 'application/json; charset=utf-8',
-    });
-
-    if (method === 'GET') {
-        const parsedURL = new URL(baseURL + url);
-        const file = JSON.parse(readFileSync(dbPath, 'utf-8'));
-
-        const queryID = parsedURL.searchParams.get('id');
-
-        if (queryID) {
-            const item = file.find((item) => item.id === parseInt(queryID));
-
-            response.write(JSON.stringify({
-                data: item
-            }));
-
-            response.end();
-        } else {
-            response.write(JSON.stringify({
-                data: file
-            }));
-
-            response.end();
-        }
-    }
-
-    if (method === 'POST') {
-        request
-            .on('data', (chunk) => {
-                const data = JSON.parse(chunk.toString());
-
-                const file = JSON.parse(readFileSync(dbPath, 'utf-8'));
-
-                const id = data.id;
-
-                const hasDBId = file.find((item) => item.id === parseInt(id));
-
-                if (hasDBId) {
-                    response.write(JSON.stringify({
-                        error: `Пользователь с id ${id} уже зарегистрирован`
-                    }));
-                } else {
-                    file.push(data);
-
-                    writeFileSync(dbPath, JSON.stringify(file));
-
-                    response.write(JSON.stringify({
-                        message: `Пользователь с id ${id} успешно зарегистрирован`
-                    }));
-                }
-            })
-            .on('end', () => {
-                response.end();
-            })
-    }
-});
-
-server.listen(8080, () => {
-    console.log('Server started on port 8080');
-});
+server.listen(8080);
